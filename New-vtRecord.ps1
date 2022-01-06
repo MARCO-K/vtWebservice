c79d6aa7b5437c7a4e2e72618748f7fb5f6da331
marco kleinert <marco.kleinert@gmail.com>
2022-01-06 18:06:05 +0100

    validate proper JSON & error handling

--- New-vtRecord.ps1
+++ New-vtRecord.ps1
@@ -20,7 +20,7 @@
       The name of the module where the new record will be created.
 
       .PARAMETER record
-      The JSOn data of the for record.
+      The JSON data of the for record.
 
       .EXAMPLE
       New-vtRecordEntry -uri $uri -contenttype $contenttype -sessionName $sessionName -module $module -record $record
@@ -28,30 +28,55 @@
       .OUTPUTS
       The cmdlet will output the resultof the create operation.
   #>
-  [CmdletBinding()]
   param(
-    [string]$uri,
-    [string]$contenttype,
-    [string]$sessionName,
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$uri,
+    [string]$contenttype = 'application/x-www-form-urlencoded',
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$sessionName,
+    [parameter(Mandatory)][ValidateSet('Campaigns','Invoice','SalesOrder','PurchaseOrder','Quotes','Vendors','PriceBooks','Calendar','Leads','Accounts','Contacts','Potentials','Products','Documents','Emails','Events','Users','Services','ModComments','Assets','Groups','Currency','DocumentFolders','CompanyDetails','LineItem','Tax','ProductTaxes')]
     [string]$module,
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][ValidateScript( {
+          if($_ | ConvertFrom-Json) 
+          {
+            $true 
+          }
+          else 
+          {
+            throw "$_ not valid JSON" 
+          }
+    }  )]
     [string]$record
   )
   begin {
+    Write-PSFMessage -Level Verbose -Message 'Starting to create a new record...'
     $create = @{
-      operation='create'
-      sessionName=$sessionName        
-      element=$record
-      elementType=$module
+      operation   = 'create'
+      sessionName = $sessionName
+      element     = $record
+      elementType = $module
     }
   }
   process {
-    try{
-      $result = Invoke-RestMethod -Uri $URI -Method 'POST' -Body $create -ContentType $contenttype
+    try
+    {
+      Write-PSFMessage -Level Verbose -Message "Trying to create a new record in $module..." 
+      $result = Invoke-RestMethod -Uri $uri -Method 'POST' -Body $create -ContentType $contenttype
+      if($result -and $result.success -eq $true)
+      {
+        $result = $result.result
+      }
+      else 
+      {
+        Write-PSFMessage -Level Warning -Message "Something went wrong... $($result.error.message)"
+        $result = $result.error.message
+      }
     }
-    catch {
+    catch 
+    {
+      Write-PSFMessage -Level Error -Message 'Something went really wrong...'
     }
   }
   end{
+    Write-PSFMessage -Level Verbose -Message 'Output the newly created record...'
     $result
-    }
+  }
 }
