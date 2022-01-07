df6025219d32b5cd9b819b64daf23e9f53d80430
marco kleinert <marco.kleinert@gmail.com>
2022-01-07 11:45:56 +0100

    accept pipeline input for record ids

--- Get-vtRetrieverecord.ps1
+++ Get-vtRetrieverecord.ps1
@@ -29,38 +29,52 @@
     [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$uri,
     [string]$contenttype = 'application/x-www-form-urlencoded',
     [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$sessionName,
-    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$recordid
+    [parameter(Mandatory,ValueFromPipeline)][ValidateNotNullOrEmpty()][string[]]$recordids
   )
   begin {
-    Write-PSFMessage -Level Verbose -Message "Starting to retrieve a record for ID: $recordid..."
-    $retrieve = @{
-      operation   = 'retrieve'
-      sessionName = $sessionName
-      id          = $recordid
-    }
+    Write-PSFMessage -Level Verbose -Message 'Starting to retrieve records...'
+
   }
   process {
-    try 
+    $results = 
+    foreach($recordid in $recordids) 
     { 
-      Write-PSFMessage -Level Verbose -Message 'Retrieving the record...'
-      $result = Invoke-RestMethod -Uri $uri -Method 'GET' -Body $retrieve -ContentType $contenttype
-      if($result -and $result.success -eq $true) 
-      {
-        $result = $result.result
+      $retrieve = @{
+        operation   = 'retrieve'
+        sessionName = $sessionName
+        id          = $recordid
       }
-      else 
-      {
-        Write-PSFMessage -Level Warning -Message "Something went wrong... $($result.error.message)"
-        $result = $result.error.message
+      try 
+      { 
+        Write-PSFMessage -Level Verbose -Message "Retrieving the record for ID: $recordid..."
+        $result = Invoke-RestMethod -Uri $uri -Method 'GET' -Body $retrieve -ContentType $contenttype
+        if($result -and $result.success -eq $true) 
+        {
+          $result.result
+        }
+        else 
+        {
+          Write-PSFMessage -Level Warning -Message "Something went wrong... $($result.error.message)"
+          $result = $result.error.message
+        }
       }
-    }
-    catch 
-    {
-      Write-PSFMessage -Level Error -Message 'Something went really wrong...'
+      catch 
+      {
+        [Management.Automation.ErrorRecord]$e = $_
+        $info = [PSCustomObject]@{
+          Exception = $e.Exception.Message
+          Reason    = $e.CategoryInfo.Reason
+          Target    = $e.CategoryInfo.TargetName
+          Script    = $e.InvocationInfo.ScriptName
+          Line      = $e.InvocationInfo.ScriptLineNumber
+          Column    = $e.InvocationInfo.OffsetInLine
+        }
+        $info
+      }
     }
   }
   end {
     Write-PSFMessage -Level Verbose -Message 'Output the record...'
-    $result
+    $results
   }
 }
