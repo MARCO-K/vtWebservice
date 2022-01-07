948de2ee48d5c092c1ec188f5a3b5cd5519beaf9
marco kleinert <marco.kleinert@gmail.com>
2022-01-07 11:05:02 +0100

    some error handling and parameter validation

--- Get-vtFieldlist.ps1
+++ Get-vtFieldlist.ps1
@@ -27,36 +27,62 @@
       The cmdlet will output all fileds as object.
   #>
   param(
-    [string]$uri,
-    [string]$contenttype,
-    [string]$sessionName,
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$uri,
+    [string]$contenttype = 'application/x-www-form-urlencoded',
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$sessionName,
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][ValidateScript({
+          if( $_ -in (Get-vtListtype -uri $uri -sessionName $sessionName)) 
+          {
+            return $true 
+          }
+          else 
+          {
+            throw "$_ is not a valid module name." 
+          }
+    } )]
     [string]$module 
   )
   
   begin {
-    #describe
+    Write-PSFMessage -Level Verbose -Message "Starting to get a field list for module $module..."
     $describe = @{
       sessionName = $sessionName
       operation   = 'describe'
       elementType = $module
     }
-    $result = Invoke-RestMethod -Uri $uri -Method 'GET' -Body $describe -ContentType $contenttype
-   
   }
   process {
-    $fieldlist = 
-    foreach ($field in $result.result.fields)
+    try 
     {
-      [PSCustomObject]@{
-        Name      = $field.Name
-        Label     = $field.label
-        Datatype  = $field.type.name
-        Mandatory = $field.mandatory
+      Write-PSFMessage -Level Verbose -Message 'Retrieving field list...'
+      $result = Invoke-RestMethod -Uri $uri -Method 'GET' -Body $describe -ContentType $contenttype
+      if($result -and $result.success -eq $true) 
+      { 
+        $fieldlist = 
+        foreach ($field in $result.result.fields)
+        {
+          [PSCustomObject]@{
+            Name      = $field.Name
+            Label     = $field.label
+            Datatype  = $field.type.name
+            Mandatory = $field.mandatory
+          }
+        }
       }
+      else 
+      {
+        Write-PSFMessage -Level Warning -Message "Something went wrong... $($result.error.message)"
+        $result = $result.error.message
+      }
+    }
+    catch 
+    {
+      Write-PSFMessage -Level Error -Message 'Something went really wrong...'
     }
   }
 
   end{
+    Write-PSFMessage -Level Verbose -Message 'Output the field list...'
     $fieldlist
   }
 }
