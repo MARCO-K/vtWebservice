84f4cfefc18db95990d224e4dfb44590c0ee1f9d
marco kleinert <marco.kleinert@gmail.com>
2022-01-07 12:02:24 +0100

    minor changes

--- Get-MD5Hash.ps1
+++ Get-MD5Hash.ps1
@@ -1,17 +1,34 @@
 ﻿function Get-MD5Hash
 {
   param(
-    [string]$tokenstring
+    [parameter(Mandatory)][ValidateNotNullOrEmpty()][string]$tokenstring
   )
   Begin {}
-  Process { 
-    $md5  = [Security.Cryptography.MD5CryptoServiceProvider]::new()
-    $utf8 = [Text.UTF8Encoding]::UTF8
-    $bytes= $md5.ComputeHash($utf8.GetBytes($tokenstring))
-    $hash = [string]::Concat($bytes.foreach{$_.ToString("x2")}) 
+  Process {
+    try 
+    {  
+      $md5  = [Security.Cryptography.MD5CryptoServiceProvider]::new()
+      $utf8 = [Text.UTF8Encoding]::UTF8
+      $bytes = $md5.ComputeHash($utf8.GetBytes($tokenstring))
+      $hash = [string]::Concat($bytes.foreach{
+          $_.ToString('x2')
+      }) 
+    }
+    catch 
+    {
+      [Management.Automation.ErrorRecord]$e = $_
+      $info = [PSCustomObject]@{
+        Exception = $e.Exception.Message
+        Reason    = $e.CategoryInfo.Reason
+        Target    = $e.CategoryInfo.TargetName
+        Script    = $e.InvocationInfo.ScriptName
+        Line      = $e.InvocationInfo.ScriptLineNumber
+        Column    = $e.InvocationInfo.OffsetInLine
+      }
+      $info
+    }
   }
   end {
-  $hash
+    $hash
   }
-
 }
